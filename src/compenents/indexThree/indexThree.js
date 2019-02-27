cdf3ab4451876591a8d885c9d7f3c951ea3d76a3
tengfeifei <545881786@qq.com>
2019-02-27 19:09:34 +0800

    ainilaodui

--- src/compenents/indexThree/indexThree.js
+++ src/compenents/indexThree/indexThree.js
@@ -1,12 +1,71 @@
 import React from 'react'
+import ReactSwipe from 'react-swipe';
+import axios from 'axios'
+import obj from './indexThree.module.scss'
+
+
 class Three extends React.Component{
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	bannerList:[],
+	  	dataList:[],
+	  	products:[],
+	  	imgList:[]
+	  };
+	}
 
 	render(){
 		return <div>
-			Three
+			{
+				this.state.bannerList.length?
+			<ReactSwipe
+        className="carousel"
+        swipeOptions={{ continuous: true, auto: 2000 ,stopPropagation: false,  disableScroll: false 	 }}
+      >
+      {this.state.bannerList.map((item)=>
+
+      	<img src={item.bannerImgSrc} key={item.id}/>
+      )}
+     
+      </ReactSwipe>
+
+      :null
+}
+
+
+
+
+		{
+			this.state.dataList.length?
+			this.state.dataList.map((item)=>
+				<div className={obj.datalist} key={item.moduleId}>
+					<div>
+						<a href="javascript">
+							<img src={item.moduleContent.bannerImgSrc}/>
+						</a>
+					</div>
+				</div>
+				)
+			:null
+		}
 
 		</div>
 	}
-}
+	componentDidMount(){
+		 
+		axios({
+			url:'/v2/page?pageId=1&tabId=10010&currentPage=1&pageSize=10&_=1551263481513'
 
+		}).then(res=>{
+			console.log(res.data.data.modules.slice(1,2))
+			this.setState({
+				bannerList:res.data.data.modules[0].moduleContent.banners,
+				dataList:res.data.data.modules.slice(1,6),
+				
+			})
+		})
+}
+}
 export default Three
